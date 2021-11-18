91cb00b80c271157e55e5f87343e8f046033264a
HanJungYoon <fpqpf02@kyobobook.com>
2021-11-18 18:04:59 +0900

    Cp3. Use JdbcTemplate

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.util.Date;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -8,6 +9,9 @@ import lombok.Data;
 
 @Data
 public class Order {
+    
+    private Long id;
+    private Date createdAt;
     
     @NotBlank(message="Name is required")
     private String deliveryName;
