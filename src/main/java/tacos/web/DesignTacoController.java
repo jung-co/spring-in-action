0d6db390f5519ffffb9f5c3165017d5d7ac7de2c
HanJungYoon <fpqpf02@kyobobook.com>
2021-12-07 11:17:12 +0900

    Cp3. SimpleJdbcInsert

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,68 +1,85 @@
 package tacos.web;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
+import tacos.Order;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
-import tacos.Taco;
 import tacos.data.IngredientRepository;
+import javax.validation.Valid;
+import org.springframework.validation.Errors;
+import tacos.data.TacoRepository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
-    
-    private final IngredientRepository ingredientRepo;
-    
-    @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepo) {
-        this.ingredientRepo = ingredientRepo;
-    }
-    
-    @GetMapping
-    public String showDesignForm(Model model) {
-        
-        List<Ingredient> ingredients = new ArrayList<>();
-        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
-        
-        Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
-        }
-        
-        model.addAttribute("taco", new Taco());
-        return "design";
-    }
-    
-    private List<Ingredient> filterByType(
-            List<Ingredient> ingredients, Type type) {
-        return ingredients.stream()
-                .filter(x -> x.getType().equals(type))
-                .collect(Collectors.toList());
-    }
-    
-    @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
-        if(errors.hasErrors()) {
-            return "design";
-        }
-        
-        // 타코 디자인을 저장한다.
-        log.info("Processing design: " + design);
-        
-        return "redirect:/orders/current";
-    }
-    
+	private final IngredientRepository ingredientRepo;
+	
+	private TacoRepository tacoRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
+	}
+	
+	@GetMapping
+	public String showDesignForm(Model model) {
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+		
+		Type[] types = Ingredient.Type.values();
+		for (Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(),
+					filterByType(ingredients, type));
+		}
+		
+		model.addAttribute("taco", new Taco());
+		
+		return "design";
+	}
+	
+	private List<Ingredient> filterByType(
+			List<Ingredient> ingredients, Type type) {
+		return ingredients
+				.stream()
+				.filter(x -> x.getType().equals(type))
+				.collect(Collectors.toList());
+	}
+	
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+	
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
+	}
+
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
+		Taco saved = tacoRepo.save(design);
+		order.addDesign(saved);
+		
+		return "redirect:/orders/current";
+	}
 }
