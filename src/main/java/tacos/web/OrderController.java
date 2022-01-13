8f3005bab8f62e27b1d162a93215e9bcc216a262
jung-co <dal_a2@naver.com>
2022-01-13 17:56:04 +0900

    Cp5. 구성 속성 사용하기 1. 자동 구성 세부 조정

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,10 +1,18 @@
 package tacos.web;
 
 import javax.validation.Valid;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -15,7 +23,14 @@ import tacos.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
+    
+    private int pageSize = 20;
+    
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
     
     private OrderRepository orderRepo;
     
@@ -59,5 +74,13 @@ public class OrderController {
         sessionStatus.setComplete();
         
         return "redirect:/";
+    }
+    
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        
+        return "orderList";
     }
 }
